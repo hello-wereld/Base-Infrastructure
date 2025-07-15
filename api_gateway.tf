f12b5de773a836abf18c4da05307c033e6e49fc5
Hello Wereld <contacthellowereld@gmail.com>
2025-07-15 23:35:53 +0000

    api gateway fix

--- api_gateway.tf
+++ api_gateway.tf
@@ -92,7 +92,7 @@ resource "aws_api_gateway_domain_name" "main" {
 resource "aws_api_gateway_base_path_mapping" "main" {
   count       = var.api_domain_name != "" ? 1 : 0
   api_id      = aws_api_gateway_rest_api.main.id
-  stage       = aws_api_gateway_deployment.main.stage_name
+  stage_name  = var.environment
   domain_name = aws_api_gateway_domain_name.main[0].domain_name
 }
 
